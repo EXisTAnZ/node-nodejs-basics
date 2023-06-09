f204d44e9b0d22b3396687cc365d2c1bce1b2f49
EXisTAnZ <ozim_ssspam@mail.ru>
2023-06-09 20:34:18 +0300

    feat: implement file creation

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+// дано:
+const text = "I am fresh and young",
+      filename = "fresh.txt",
+      foldername = "files",
+      errmsg = "FS operation failed";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url)),
+      folderpath = path.join(__dirname, foldername),
+      filepath = path.join(folderpath, filename);
+
 const create = async () => {
-    // Write your code here 
+    fs.writeFile(filepath, text, { flag: 'wx' }, (error) => { if (error) throw new Error(errmsg); });
 };
 
 await create();

98856918a0c1ccca72c68da4328ec3ee6354b975
EXisTAnZ <ozim_ssspam@mail.ru>
2023-06-09 22:11:36 +0300

    feat: implement directory copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import { throws } from "assert";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+// дано:
+const fromFolderName = "files",
+      toFolderName = "files_copy",
+      errmsg = "FS operation failed";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url)),
+      fromFolder = path.join(__dirname, fromFolderName),
+      toFolder = path.join(__dirname, toFolderName);
+
 const copy = async () => {
-    // Write your code here 
+    const opts = { recursive: true, errorOnExist: true, force: false };
+    fs.cp(fromFolder, toFolder, opts, (error) => { if (error) throw new Error(errmsg) });
 };
 
 await copy();

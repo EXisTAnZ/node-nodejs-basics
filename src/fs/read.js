f15f77ee49bceccd61ad1ed94d9ef02f12d73cf2
EXisTAnZ <ozim_ssspam@mail.ru>
2023-06-13 14:29:00 +0300

    feat: implement fs read file text content

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+// дано:
+const folderName = "files",
+      readFileName = "fileToRead.txt",
+      errmsg = "FS operation failed";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url)),
+      readFilePath = path.join(__dirname, folderName, readFileName);
+
 const read = async () => {
-    // Write your code here 
+    fs.readFile(readFilePath, { encoding:"utf-8", flag:"r" })
+      .then(content => console.log(content))
+      .catch(err => { throw new Error(errmsg); });
 };
 
 await read();

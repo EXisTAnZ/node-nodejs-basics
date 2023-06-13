e11f97ed299dbd6eb91b8494d2505497df18b5fc
EXisTAnZ <ozim_ssspam@mail.ru>
2023-06-13 16:44:26 +0300

    feat: implement reading arguments from cline

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    console.log(
+        args.map((arg,idx) => idx%2 ? '' :`${arg.slice(2)} is ${args[idx+1]}`)
+            .filter(item => item)
+            .join(', '));
 };
 
 parseArgs();

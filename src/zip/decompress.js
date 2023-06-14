0ee026097b4498d164b7cca551d25a92984a06c2
EXisTAnZ <ozim_ssspam@mail.ru>
2023-06-14 22:44:23 +0300

    feat: implement stream decompresser

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { createGunzip } from "zlib";
+
+// дано:
+const folderName = "files",
+      fileName = "fileToCompress.txt",
+      archName = "archive.gz",
+      errmsg = "FS operation failed";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url)),
+      filePath = path.join(__dirname, folderName, fileName),
+      archPath = path.join(__dirname, folderName, archName);
+
 const decompress = async () => {
-    // Write your code here 
+    const unzipper = createGunzip(),
+    readStream = new fs.ReadStream(archPath),
+    outStream = new fs.WriteStream(filePath);
+
+    readStream.pipe(unzipper).pipe(outStream);
 };
 
 await decompress();

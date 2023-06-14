3561a2d7b41e69bf2575e49662b03bc357c78592
EXisTAnZ <ozim_ssspam@mail.ru>
2023-06-14 22:24:53 +0300

    feat: implement stream archiever

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { createGzip } from "zlib";
+
+// дано:
+const folderName = "files",
+      fileName = "fileToCompress.txt",
+      archName = "archive.gz",
+      errmsg = "FS operation failed";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url)),
+      filePath = path.join(__dirname, folderName, fileName),
+      archPath = path.join(__dirname, folderName, archName);
+
 const compress = async () => {
-    // Write your code here 
+    const zipper = createGzip(),
+        outStream = new fs.WriteStream(archPath),
+        readStream = new fs.ReadStream(filePath);
+    readStream.pipe(zipper).pipe(outStream);
 };
 
 await compress();

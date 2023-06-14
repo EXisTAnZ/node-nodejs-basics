1678c0ce2cf0dbd072de916cc7f83c881a57127c
EXisTAnZ <ozim_ssspam@mail.ru>
2023-06-14 17:30:49 +0300

    feat: implement read stream from file to stdout

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// дано:
+const folderName = "files",
+      readFileName = "fileToRead.txt",
+      errmsg = "FS operation failed";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url)),
+      readFilePath = path.join(__dirname, folderName, readFileName);
+
 const read = async () => {
-    // Write your code here 
+    const stream = new fs.ReadStream(readFilePath);
+    stream.on('error', () => { throw new Error(errmsg) });
+    stream.pipe(process.stdout);
 };
 
 await read();

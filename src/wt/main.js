6bfdbded4719e150df6e5cc705357ae24a5a321c
EXisTAnZ <ozim_ssspam@mail.ru>
2023-06-15 16:51:25 +0300

    feat: implement worker threads for each cpu

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import fs from "fs";
+import path from "path";
+import os from "os";
+import { fileURLToPath } from "url";
+import { Worker } from "node:worker_threads";
+
+
+// дано:
+const workerFileName = "worker.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url)),
+      workerFilePath = path.join(__dirname, workerFileName);
+
+const res = [];
+
+const sendResult = async (result, id) => {
+    if(result) res[id] = { status: "resolved", data: result };
+    else res[id] = { status: "error", data: null };
+    if (res.length == os.cpus().length && res.every(item => !!item.status)) console.log(res);
+}
+
 const performCalculations = async () => {
-    // Write your code here
+    for (let i = 0; i<os.cpus().length; i++) {
+        const worker = new Worker(workerFilePath);
+        worker.postMessage(i+10);
+        worker.on("message", (fibo) => sendResult(fibo, i));
+        worker.on("error", () => sendResult());
+    }
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,15 @@
-// n should be received from main thread
+import { parentPort } from "worker_threads";
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
+let res = 0;
+parentPort.on("message", (n) => {
+    res = nthFibonacci(n);
+    sendResult();
+})
+
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    if (res) parentPort.postMessage(res);
 };
 
 sendResult();

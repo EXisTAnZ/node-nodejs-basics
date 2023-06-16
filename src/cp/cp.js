0683263ebc0071ccf35039104be71381b7a0f0a1
EXisTAnZ <ozim_ssspam@mail.ru>
2023-06-16 15:18:32 +0300

    feat: implement spawn of child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import path from "path";
+import { fileURLToPath } from "url";
+import { spawn } from "child_process";
+
+// дано:
+const folderName = "files", cpFileName = "script.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url)),
+      cpFilePath = path.join(__dirname, folderName, cpFileName);
+
+const spawnChildProcess = async (...args) => {
+    const cp = spawn("node", [cpFilePath, ...args]);
+    process.stdin.pipe(cp.stdin);
+    cp.stdout.pipe(process.stdout);                                                                                                                                                                                                                                                                                                                
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess('deadlyArgument', 'and', 'deadlyFact');
